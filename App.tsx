debde13f2740902cfda6a6acf970b3a9792b3959
unknown <raquibrfy@gmail.com>
2023-05-31 19:28:19 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'react-native-gesture-handler'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import MainNavigator from './src/navigator/MainNavigator';
+import TryCodeScreen from './src/screens/TryCodeScreen';
+import NoteListScreen from './src/screens/NoteListScreen';
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <MainNavigator/>
+    </SafeAreaProvider>
+  )
+}
+export default App;
